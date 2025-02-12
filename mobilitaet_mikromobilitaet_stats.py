c5b925fbaa59f1a609805839b928a07ecdf46349
Orhan Saeedi <orhan.saeedi@bs.ch>
2025-02-12 09:48:36 +0100

    Add stats version

--- mobilitaet_mikromobilitaet_stats.py
+++ mobilitaet_mikromobilitaet_stats.py
@@ -1,8 +1,9 @@
 """
-# mobilitaet_mikromobilitaet
+# mobilitaet_mikromobilitaet_stats
 This DAG updates the following datasets:
 
-- [100413](https://data.bs.ch/explore/dataset/100413)
+- [100414](https://data.bs.ch/explore/dataset/100414)
+- [100418](https://data.bs.ch/explore/dataset/100418)
 """
 
 from airflow import DAG
@@ -12,9 +13,9 @@ from docker.types import Mount
 
 default_args = {
     'owner': 'orhan.saeedi',
-    'description': 'Run the mobilitaet_mikromobilitaet docker container',
+    'description': 'Run the mobilitaet_mikromobilitaet_stats docker container',
     'depend_on_past': False,
-    'start_date': datetime(2025, 1, 31),
+    'start_date': datetime(2025, 2, 12),
     'email': ["jonas.bieri@bs.ch", "orhan.saeedi@bs.ch", "rstam.aloush@bs.ch", "renato.farruggio@bs.ch"],
     'email_on_failure': True,
     'email_on_retry': False,
@@ -22,15 +23,15 @@ default_args = {
     'retry_delay': timedelta(minutes=3)
 }
 
-with DAG('mobilitaet_mikromobilitaet', default_args=default_args, schedule_interval="*/10 * * * *", catchup=False) as dag:
+with DAG('mobilitaet_mikromobilitaet_stats', default_args=default_args, schedule_interval="0 4 * * *", catchup=False) as dag:
     dag.doc_md = __doc__
     process_upload = DockerOperator(
         task_id='process-upload',
-        image='mobilitaet_mikromobilitaet:latest',
+        image='mobilitaet_mikromobilitaet_stats:latest',
         api_version='auto',
         auto_remove='force',
-        command='python3 -m mobilitaet_mikromobilitaet.src.etl',
-        container_name='mobilitaet_mikromobilitaet',
+        command='python3 -m mobilitaet_mikromobilitaet.src.etl_stats',
+        container_name='mobilitaet_mikromobilitaet_stats',
         docker_url="unix://var/run/docker.sock",
         network_mode="bridge",
         tty=True,
