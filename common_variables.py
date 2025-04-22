fc53c85992e0221b275ca3b6cfcc3e3981bce98d
Orhan Saeedi <orhan.saeedi@bs.ch>
2025-04-22 17:15:39 +0200

    Add no_proxy

--- common_variables.py
+++ common_variables.py
@@ -5,6 +5,7 @@ PATH_TO_CODE = Variable.get("PATH_TO_CODE")
 COMMON_ENV_VARS = {
     "https_proxy": Variable.get("https_proxy"),
     "http_proxy": Variable.get("http_proxy"),
+    "no_proxy": Variable.get("no_proxy"),
     "EMAIL_RECEIVERS": Variable.get("EMAIL_RECEIVERS"),
     "EMAIL_SERVER": Variable.get("EMAIL_SERVER"),
     "EMAIL": Variable.get("EMAIL"),
