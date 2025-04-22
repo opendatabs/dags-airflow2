d869977822039bbc08bea3f940b090a58806d13a
Orhan Saeedi <orhan.saeedi@bs.ch>
2025-04-22 17:01:12 +0200

    Refactor ibs_parkhaus_bewegungen

--- ibs_parkhaus_bewegungen.py
+++ ibs_parkhaus_bewegungen.py
@@ -6,36 +6,61 @@ This DAG updates the following datasets:
 
 """
 
-from airflow import DAG
 from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.models import Variable
 from airflow.providers.docker.operators.docker import DockerOperator
 from docker.types import Mount
 
+from common_variables import COMMON_ENV_VARS, PATH_TO_CODE
+
 default_args = {
-    'owner': 'orhan.saeedi',
-    'description': 'Run the ibs_parkhaus_bewegungen.py docker container',
-    'depend_on_past': False,
-    'start_date': datetime(2024, 2, 2),
-    'email': ["jonas.bieri@bs.ch", "orhan.saeedi@bs.ch", "rstam.aloush@bs.ch", "renato.farruggio@bs.ch"],
-    'email_on_failure': True,
-    'email_on_retry': False,
-    'retries': 0,
-    'retry_delay': timedelta(minutes=15)
+    "owner": "orhan.saeedi",
+    "depend_on_past": False,
+    "start_date": datetime(2024, 2, 2),
+    "email": Variable.get("EMAIL_RECEIVERS"),
+    "email_on_failure": True,
+    "email_on_retry": False,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=15),
 }
 
-with DAG('ibs_parkhaus_bewegungen', default_args=default_args, schedule_interval='0 1 * * *', catchup=False) as dag:
+with DAG(
+    "ibs_parkhaus_bewegungen",
+    default_args=default_args,
+    description="run the ibs_parkhaus_bewegungen docker container",
+    schedule_interval="0 1 * * *",
+    catchup=False,
+) as dag:
     dag.doc_md = __doc__
     upload = DockerOperator(
-        task_id='upload',
-        image='ibs_parkhaus_bewegungen:latest',
-        api_version='auto',
-        auto_remove='force',
-        command='python3 -m ibs_parkhaus_bewegungen.etl',
-        container_name='ibs_parkhaus_bewegungen--upload',
+        task_id="upload",
+        image="ghcr.io/opendatabs/data-processing/ibs_parkhaus_bewegungen:latest",
+        force_pull=True,
+        api_version="auto",
+        auto_remove="force",
+        command="uv run -m etl",
+        private_environment=COMMON_ENV_VARS,
+        container_name="ibs_parkhaus_bewegungen--upload",
         docker_url="unix://var/run/docker.sock",
         network_mode="bridge",
         tty=True,
-        mounts=[Mount(source="/data/dev/workspace/data-processing", target="/code/data-processing", type="bind"),
-                Mount(source="/mnt/OGD-DataExch/ibs-parkhaeuser/Ein-Ausfahrten",
-                      target="/code/data-processing/ibs_parkhaus_bewegungen/data_orig", type="bind")]
+        mounts=[
+            Mount(
+                source=f"{PATH_TO_CODE}/data-processing/ibs_parkhaus_bewegungen/data",
+                target="/code/data",
+                type="bind",
+            ),
+            Mount(
+                source="/mnt/OGD-DataExch/ibs-parkhaeuser/Ein-Ausfahrten",
+                target="/code/data_orig",
+                type="bind",
+            ),
+            Mount(
+                source=f"{PATH_TO_CODE}/data-processing/ibs_parkhaus_bewegungen/change_tracking",
+                target="/code/change_tracking",
+                type="bind",
+            ),
+        ],
     )
