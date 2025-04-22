a48c0b1ca88705dd10284e2f729759446047e0df
Orhan Saeedi <orhan.saeedi@bs.ch>
2025-04-22 16:30:50 +0200

    Refactor gd_awbassermonitoring

--- gd_abwassermonitoring.py
+++ gd_abwassermonitoring.py
@@ -5,36 +5,61 @@ This DAG updates the following datasets:
 - [100302](https://data.bs.ch/explore/dataset/100302)
 
 """
-from airflow import DAG
+
 from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.models import Variable
 from airflow.providers.docker.operators.docker import DockerOperator
 from docker.types import Mount
 
+from common_variables import COMMON_ENV_VARS, PATH_TO_CODE
+
+
 default_args = {
-    'owner': 'hester.pieters',
-    'description': 'Run the gd_abwassermonitoring.py docker container',
-    'depend_on_past': False,
-    'start_date': datetime(2024, 1, 26),
-    'email': ["jonas.bieri@bs.ch", "orhan.saeedi@bs.ch", "rstam.aloush@bs.ch", "renato.farruggio@bs.ch"],
-    'email_on_failure': True,
-    'email_on_retry': False,
-    'retries': 0,
-    'retry_delay': timedelta(minutes=15)
+    "owner": "hester.pieters",
+    "depend_on_past": False,
+    "start_date": datetime(2024, 1, 26),
+    "email": Variable.get("EMAIL_RECEIVERS"),
+    "email_on_failure": True,
+    "email_on_retry": False,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=15),
 }
 
-with DAG('gd_abwassermonitoring', default_args=default_args, schedule_interval="0 * * * *", catchup=False) as dag:
+with DAG(
+    "gd_abwassermonitoring",
+    default_args=default_args,
+    description="Run the gd_abwassermonitoring.py docker container",
+    schedule_interval="0 * * * *",
+    catchup=False,
+) as dag:
     dag.doc_md = __doc__
     upload = DockerOperator(
-        task_id='upload',
-        image='gd_abwassermonitoring:latest',
-        api_version='auto',
-        auto_remove='force',
-        command='python3 -m gd_abwassermonitoring.etl',
-        container_name='gd_abwassermonitoring--upload',
+        task_id="upload",
+        image="ghcr.io/opendatabs/data-processing/gd_abwassermonitoring:latest",
+        force_pull=True,
+        api_version="auto",
+        auto_remove="force",
+        command="uv run -m etl",
+        private_environment={
+            **COMMON_ENV_VARS,
+            "ODS_PUSH_URL_100302": Variable.get("ODS_PUSH_URL_100302"),
+        },
+        container_name="gd_abwassermonitoring--upload",
         docker_url="unix://var/run/docker.sock",
         network_mode="bridge",
         tty=True,
-        mounts=[Mount(source="/data/dev/workspace/data-processing", target="/code/data-processing", type="bind"),
-                Mount(source="/mnt/OGD-DataExch/GD-Kantonslabor/Influenza_RSV_Abwasser",
-                      target="/code/data-processing/gd_abwassermonitoring/data", type="bind")]
+        mounts=[
+            Mount(
+                source="/mnt/OGD-DataExch/GD-Kantonslabor/Influenza_RSV_Abwasser",
+                target="/code/data",
+                type="bind",
+            ),
+            Mount(
+                source=f"{PATH_TO_CODE}/data-processing/gd_abwassermonitoring/change_tracking",
+                target="/code/change_tracking",
+                type="bind",
+            ),
+        ],
     )
