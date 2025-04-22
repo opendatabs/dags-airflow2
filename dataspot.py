3ec04d76cb58ce8e51f10a1e145e07482a00de2d
Orhan Saeedi <orhan.saeedi@bs.ch>
2025-04-22 15:34:10 +0200

    Refactor dataspot

--- dataspot.py
+++ dataspot.py
@@ -1,40 +1,63 @@
 """
 # dataspot
 """
-from airflow import DAG
+
 from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.models import Variable
 from airflow.providers.docker.operators.docker import DockerOperator
 from docker.types import Mount
-from airflow.models import Variable
 
-# This is set in the Airflow UI under Admin -> Variables
-https_proxy = Variable.get("https_proxy")
+from common_variables import COMMON_ENV_VARS, PATH_TO_CODE
 
 default_args = {
-    'owner': 'rstam.aloush',
-    'description': 'Run the dataspot docker container',
-    'depend_on_past': False,
-    'start_date': datetime(2024, 8, 19),
-    'email': ["jonas.bieri@bs.ch", "orhan.saeedi@bs.ch", "rstam.aloush@bs.ch", "renato.farruggio@bs.ch"],
-    'email_on_failure': True,
-    'email_on_retry': False,
-    'retries': 0,
-    'retry_delay': timedelta(minutes=15)
+    "owner": "rstam.aloush",
+    "description": "Run the dataspot docker container",
+    "depend_on_past": False,
+    "start_date": datetime(2024, 8, 19),
+    "email": Variable.get("EMAIL_RECEIVERS"),
+    "email_on_failure": True,
+    "email_on_retry": False,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=15),
 }
 
-with DAG('dataspot', default_args=default_args, schedule_interval="0 */2 * * *", catchup=False) as dag:
+with DAG(
+    "dataspot",
+    default_args=default_args,
+    description="Run the dataspot docker container",
+    schedule_interval="0 */2 * * *",
+    catchup=False,
+) as dag:
     dag.doc_md = __doc__
     upload = DockerOperator(
-        task_id='upload',
-        image='ghcr.io/opendatabs/data-processing/dataspot:latest',
+        task_id="upload",
+        image="ghcr.io/opendatabs/data-processing/dataspot:latest",
         force_pull=True,
-        api_version='auto',
-        auto_remove='force',
-        environment={'https_proxy': https_proxy},
-        command='uv run -m etl',
-        container_name='dataspot',
+        api_version="auto",
+        auto_remove="force",
+        private_environment=COMMON_ENV_VARS,
+        command="uv run -m src.etl",
+        container_name="dataspot",
         docker_url="unix://var/run/docker.sock",
         network_mode="bridge",
         tty=True,
-        mounts=[Mount(source="/data/dev/workspace/data-processing", target="/code/data-processing", type="bind")]
+        mounts=[
+            Mount(
+                source=f"{PATH_TO_CODE}/data-processing/dataspot/data",
+                target="/code/data",
+                type="bind",
+            ),
+            Mount(
+                source=f"{PATH_TO_CODE}/data-processing/dataspot/change_tracking",
+                target="/code/change_tracking",
+                type="bind",
+            ),
+            Mount(
+                source=f"{PATH_TO_CODE}/data-processing/dataspot/.dataspot.env",
+                target="/code/.dataspot.env",
+                type="bind",
+            ),
+        ],
     )
