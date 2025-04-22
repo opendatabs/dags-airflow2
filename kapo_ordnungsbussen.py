bed96c7f511304385ed93c7eb236a83369550f1c
Orhan Saeedi <orhan.saeedi@bs.ch>
2025-04-22 19:54:44 +0200

    Refactor kapo_ordnungsbussen

--- kapo_ordnungsbussen.py
+++ kapo_ordnungsbussen.py
@@ -4,36 +4,62 @@ This DAG updates the following datasets:
 
 - [100058](https://data.bs.ch/explore/dataset/100058)
 """
-from airflow import DAG
+
 from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.models import Variable
 from airflow.providers.docker.operators.docker import DockerOperator
 from docker.types import Mount
 
+from common_variables import COMMON_ENV_VARS, PATH_TO_CODE
+
 default_args = {
-    'owner': 'jonas.bieri',
-    'description': 'Run the kapo_ordnungsbussen docker container',
-    'depend_on_past': False,
-    'start_date': datetime(2024, 1, 26),
-    'email': ["jonas.bieri@bs.ch", "orhan.saeedi@bs.ch", "rstam.aloush@bs.ch", "renato.farruggio@bs.ch"],
-    'email_on_failure': True,
-    'email_on_retry': False,
-    'retries': 0,
-    'retry_delay': timedelta(minutes=15)
+    "owner": "jonas.bieri",
+    "depend_on_past": False,
+    "start_date": datetime(2024, 1, 26),
+    "email": Variable.get("EMAIL_RECEIVERS"),
+    "email_on_failure": True,
+    "email_on_retry": False,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=15),
 }
 
-with DAG('kapo_ordnungsbussen', default_args=default_args, schedule_interval="0 11 * * *", catchup=False) as dag:
+with DAG(
+    "kapo_ordnungsbussen",
+    default_args=default_args,
+    description="Run the kapo_ordnungsbussen docker container",
+    schedule_interval="0 11 * * *",
+    catchup=False,
+) as dag:
     dag.doc_md = __doc__
     upload = DockerOperator(
-        task_id='upload',
-        image='kapo_ordnungsbussen:latest',
-        api_version='auto',
-        auto_remove='force',
-        command='python3 -m kapo_ordnungsbussen.src.etl',
-        container_name='kapo_ordnungsbussen--upload',
+        task_id="upload",
+        image="ghcr.io/opendatabs/data-processing/kapo_ordnungsbussen:latest",
+        force_pull=True,
+        api_version="auto",
+        auto_remove="force",
+        command="uv run -m src.etl",
+        private_environment=COMMON_ENV_VARS,
+        container_name="kapo_ordnungsbussen--upload",
         docker_url="unix://var/run/docker.sock",
         network_mode="bridge",
         tty=True,
-        mounts=[Mount(source="/data/dev/workspace/data-processing", target="/code/data-processing", type="bind"),
-                Mount(source="/mnt/OGD-DataExch/KaPo/Ordnungsbussen",
-                      target="/code/data-processing/kapo_ordnungsbussen/data_orig", type="bind")]
+        mounts=[
+            Mount(
+                source=f"{PATH_TO_CODE}/data-processing/kapo_ordnungsbussen/data",
+                target="/code/data",
+                type="bind",
+            ),
+            Mount(
+                source="/mnt/OGD-DataExch/KaPo/Ordnungsbussen",
+                target="/code/data_orig",
+                type="bind",
+            ),
+            Mount(
+                source=f"{PATH_TO_CODE}/data-processing/kapo_ordnungsbussen/change_tracking",
+                target="/code/change_tracking",
+                type="bind",
+            ),
+        ],
     )
