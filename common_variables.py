e7a377f691776dc0f0733033ce6aef36f262a265
Orhan Saeedi <orhan.saeedi@bs.ch>
2025-04-22 10:37:02 +0200

    Create common_variables.py

---
+++ common_variables.py
@@ -0,0 +1,15 @@
+from airflow.models import Variable
+
+# Variables are set in the Airflow UI under Admin -> Variables
+PATH_TO_CODE = Variable.get("PATH_TO_CODE")
+COMMON_ENV_VARS = {
+    "https_proxy": Variable.get("https_proxy"),
+    "http_proxy": Variable.get("http_proxy"),
+    "EMAIL_RECEIVERS": Variable.get("EMAIL_RECEIVERS"),
+    "EMAIL_SERVER": Variable.get("EMAIL_SERVER"),
+    "EMAIL": Variable.get("EMAIL"),
+    "FTP_SERVER": Variable.get("FTP_SERVER"),
+    "FTP_USER": Variable.get("FTP_USER"),
+    "FTP_PASS": Variable.get("FTP_PASS"),
+    "ODS_API_KEY": Variable.get("ODS_API_KEY"),
+}
