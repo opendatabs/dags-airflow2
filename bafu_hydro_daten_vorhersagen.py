bbcd1585d4cf2e1730061eeb2f952e14b764dddf
Orhan Saeedi <orhan.saeedi@bs.ch>
2025-04-22 13:39:38 +0200

    Refactor bafu_hydro_daten_vorhersagen

--- bafu_hydro_daten_vorhersagen.py
+++ bafu_hydro_daten_vorhersagen.py
@@ -5,34 +5,62 @@ This DAG updates the following datasets:
 - [100271](https://data.bs.ch/explore/dataset/100271)
 - [100272](https://data.bs.ch/explore/dataset/100272)
 """
-from airflow import DAG
+
 from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.models import Variable
 from airflow.providers.docker.operators.docker import DockerOperator
 from docker.types import Mount
 
+from common_variables import COMMON_ENV_VARS, PATH_TO_CODE
+
 default_args = {
-    'owner': 'jonas.bieri',
-    'description': 'Run the bafu_hydrodaten_vorhersagen docker container',
-    'depend_on_past': False,
-    'start_date': datetime(2024, 1, 22),
-    'email': ["jonas.bieri@bs.ch", "orhan.saeedi@bs.ch", "rstam.aloush@bs.ch", "renato.farruggio@bs.ch"],
-    'email_on_failure': True,
-    'email_on_retry': False,
-    'retries': 0,
-    'retry_delay': timedelta(minutes=3)
+    "owner": "jonas.bieri",
+    "depend_on_past": False,
+    "start_date": datetime(2024, 1, 22),
+    "email": Variable.get("EMAIL_RECEIVERS"),
+    "email_on_failure": True,
+    "email_on_retry": False,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=3),
 }
 
-with DAG('bafu_hydrodaten_vorhersagen', default_args=default_args, schedule_interval="0 * * * *", catchup=False) as dag:
+with DAG(
+    "bafu_hydrodaten_vorhersagen",
+    default_args=default_args,
+    description="Run the bafu_hydrodaten_vorhersagen docker container",
+    schedule_interval="0 * * * *",
+    catchup=False,
+) as dag:
     dag.doc_md = __doc__
     upload = DockerOperator(
-        task_id='upload',
-        image='bafu_hydrodaten_vorhersagen:latest',
-        api_version='auto',
-        auto_remove='force',
-        command='python3 -m bafu_hydrodaten_vorhersagen.etl',
-        container_name='bafu_hydrodaten_vorhersagen',
+        task_id="upload",
+        image="ghcr.io/opendatabs/data-processing/bafu_hydrodaten_vorhersagen:latest",
+        force_pull=True,
+        api_version="auto",
+        auto_remove="force",
+        command="uv run -m etl",
+        private_environment={
+            **COMMON_ENV_VARS,
+            "DICT_URL_BAFU_VORHERSAGEN": Variable.get("DICT_URL_BAFU_VORHERSAGEN"),
+            "HTTPS_USER_01": Variable.get("HTTPS_USER_01"),
+            "HTTPS_PASS_01": Variable.get("HTTPS_PASS_01"),
+        },
+        container_name="bafu_hydrodaten_vorhersagen",
         docker_url="unix://var/run/docker.sock",
         network_mode="bridge",
         tty=True,
-        mounts=[Mount(source="/data/dev/workspace/data-processing", target="/code/data-processing", type="bind")]
+        mounts=[
+            Mount(
+                source=f"{PATH_TO_CODE}/data-processing/bafu_hydrodaten_vorhersagen/data",
+                target="/code/data",
+                type="bind",
+            ),
+            Mount(
+                source=f"{PATH_TO_CODE}/data-processing/bafu_hydrodaten_vorhersagen/change_tracking",
+                target="/code/change_tracking",
+                type="bind",
+            ),
+        ],
     )
