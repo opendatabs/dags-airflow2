87a83cb003d1a1b39b837c7db73363a6f9fdffdd
Orhan Saeedi <orhan.saeedi@bs.ch>
2025-04-22 19:10:19 +0200

    Refactor jfs_gartenbaeder

--- jfs_gartenbaeder.py
+++ jfs_gartenbaeder.py
@@ -4,40 +4,57 @@ This DAG updates the following datasets:
 
 - [100384](https://data.bs.ch/explore/dataset/100384)
 """
-from airflow import DAG
+
 from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.models import Variable
 from airflow.providers.docker.operators.docker import DockerOperator
 from docker.types import Mount
-from airflow.models import Variable
 
-# This is set in the Airflow UI under Admin -> Variables
-https_proxy = Variable.get("https_proxy")
+from common_variables import COMMON_ENV_VARS, PATH_TO_CODE
 
 default_args = {
-    'owner': 'rstam.aloush',
-    'description': 'Run the jfs_gartenbaeder docker container',
-    'depend_on_past': False,
-    'start_date': datetime(2024, 8, 14),
-    'email': ["jonas.bieri@bs.ch", "orhan.saeedi@bs.ch", "rstam.aloush@bs.ch", "renato.farruggio@bs.ch"],
-    'email_on_failure': True,
-    'email_on_retry': False,
-    'retries': 0,
-    'retry_delay': timedelta(minutes=15)
+    "owner": "rstam.aloush",
+    "depend_on_past": False,
+    "start_date": datetime(2024, 8, 14),
+    "email": Variable.get("EMAIL_RECEIVERS"),
+    "email_on_failure": True,
+    "email_on_retry": False,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=15),
 }
 
-with DAG('jfs_gartenbaeder', default_args=default_args, schedule_interval="*/15 * * * *", catchup=False) as dag:
+with DAG(
+    "jfs_gartenbaeder",
+    description="Run the jfs_gartenbaeder docker container",
+    default_args=default_args,
+    schedule_interval="*/15 * * * *",
+    catchup=False,
+) as dag:
     dag.doc_md = __doc__
     upload = DockerOperator(
-        task_id='upload',
-        image='ghcr.io/opendatabs/data-processing/jfs_gartenbaeder:latest',
+        task_id="upload",
+        image="ghcr.io/opendatabs/data-processing/jfs_gartenbaeder:latest",
         force_pull=True,
-        api_version='auto',
-        auto_remove='force',
-        environment={'https_proxy': https_proxy},
-        command='uv run -m etl',
-        container_name='jfs_gartenbaeder',
+        api_version="auto",
+        auto_remove="force",
+        private_environment=COMMON_ENV_VARS,
+        command="uv run -m etl",
+        container_name="jfs_gartenbaeder",
         docker_url="unix://var/run/docker.sock",
         network_mode="bridge",
         tty=True,
-        mounts=[Mount(source="/data/dev/workspace/data-processing", target="/code/data-processing", type="bind")]
+        mounts=[
+            Mount(
+                source=f"{PATH_TO_CODE}/data-processing/jfs_gartenbaeder/data",
+                target="/code/data",
+                type="bind",
+            ),
+            Mount(
+                source=f"{PATH_TO_CODE}/data-processing/jfs_gartenbaeder/change_tracking",
+                target="/code/change_tracking",
+                type="bind",
+            ),
+        ],
     )
