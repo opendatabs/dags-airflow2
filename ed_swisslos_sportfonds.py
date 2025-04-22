57498130f0e88e0f886dfcfed0a7d4ba48c3c526
Orhan Saeedi <orhan.saeedi@bs.ch>
2025-04-22 15:49:18 +0200

    Refactor ed_swisslos_sportfonds

--- ed_swisslos_sportfonds.py
+++ ed_swisslos_sportfonds.py
@@ -5,37 +5,61 @@ This DAG updates the following datasets:
 - [100221](https://data.bs.ch/explore/dataset/100221)
 """
 
-from airflow import DAG
 from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.models import Variable
 from airflow.providers.docker.operators.docker import DockerOperator
 from docker.types import Mount
 
+from common_variables import COMMON_ENV_VARS, PATH_TO_CODE
+
 default_args = {
-    'owner': 'orhan.saeedi',
-    'description': 'Run the ed_swisslos_sportfonds docker container',
-    'depend_on_past': False,
-    'start_date': datetime(2024, 2, 2),
-    'email': ["jonas.bieri@bs.ch", "orhan.saeedi@bs.ch", "rstam.aloush@bs.ch", "renato.farruggio@bs.ch"],
-    'email_on_failure': True,
-    'email_on_retry': False,
-    'retries': 0,
-    'retry_delay': timedelta(minutes=15)
+    "owner": "orhan.saeedi",
+    "depend_on_past": False,
+    "start_date": datetime(2024, 2, 2),
+    "email": Variable.get("EMAIL_RECEIVERS"),
+    "email_on_failure": True,
+    "email_on_retry": False,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=15),
 }
 
-with DAG('ed_swisslos_sportfonds', default_args=default_args, schedule_interval='0 2 * * *',
-         catchup=False) as dag:
+with DAG(
+    "ed_swisslos_sportfonds",
+    default_args=default_args,
+    description="Run the ed_swisslos_sportfonds docker container",
+    schedule_interval="0 2 * * *",
+    catchup=False,
+) as dag:
     dag.doc_md = __doc__
     upload = DockerOperator(
-        task_id='upload',
-        image='ed_swisslos_sportfonds:latest',
-        api_version='auto',
-        auto_remove='force',
-        command='python3 -m ed_swisslos_sportfonds.etl',
-        container_name='ed_swisslos_sportfonds',
+        task_id="upload",
+        image="ghcr.io/opendatabs/data-processing/ed_swisslos_sportfonds:latest",
+        force_pull=True,
+        api_version="auto",
+        auto_remove="force",
+        command="uv run -m etl",
+        private_environment=COMMON_ENV_VARS,
+        container_name="ed_swisslos_sportfonds",
         docker_url="unix://var/run/docker.sock",
         network_mode="bridge",
         tty=True,
-        mounts=[Mount(source="/data/dev/workspace/data-processing", target="/code/data-processing", type="bind"),
-                Mount(source="/mnt/OGD-DataExch/ED-Swisslos-Sportfonds",
-                      target="/code/data-processing/ed_swisslos_sportfonds/data_orig", type="bind")]
+        mounts=[
+            Mount(
+                source=f"{PATH_TO_CODE}/data-processing/ed_swisslos_sportfonds/data",
+                target="/code/data",
+                type="bind",
+            ),
+            Mount(
+                source="/mnt/OGD-DataExch/ED-Swisslos-Sportfonds",
+                target="/code/data_orig",
+                type="bind",
+            ),
+            Mount(
+                source=f"{PATH_TO_CODE}/data-processing/ed_swisslos_sportfonds/change_tracking",
+                target="/code/change_tracking",
+                type="bind",
+            ),
+        ],
     )
