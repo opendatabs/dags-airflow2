4e7533aa422f3b0c51589cf8d64d64dc9ffd5b17
Orhan Saeedi <orhan.saeedi@bs.ch>
2025-04-22 20:13:30 +0200

    Refactor lufthygiene_rosental

--- lufthygiene_rosental.py
+++ lufthygiene_rosental.py
@@ -5,34 +5,56 @@ This DAG updates the following dataset:
 - [100275](https://data.bs.ch/explore/dataset/100275)
 """
 
-from airflow import DAG
 from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.models import Variable
 from airflow.providers.docker.operators.docker import DockerOperator
 from docker.types import Mount
 
+from common_variables import COMMON_ENV_VARS, PATH_TO_CODE
+
 default_args = {
-    'owner': 'hester.pieters',
-    'description': 'Run the lufthygiene_rosental docker container',
-    'depend_on_past': False,
-    'start_date': datetime(2024, 1, 29),
-    'email': ["jonas.bieri@bs.ch", "orhan.saeedi@bs.ch", "rstam.aloush@bs.ch", "renato.farruggio@bs.ch"],
-    'email_on_failure': True,
-    'email_on_retry': False,
-    'retries': 0,
-    'retry_delay': timedelta(minutes=3)
+    "owner": "hester.pieters",
+    "depend_on_past": False,
+    "start_date": datetime(2024, 1, 29),
+    "email": Variable.get("EMAIL_RECEIVERS"),
+    "email_on_failure": True,
+    "email_on_retry": False,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=3),
 }
 
-with DAG('lufthygiene_rosental', default_args=default_args, schedule_interval="*/30 * * * *", catchup=False) as dag:
+with DAG(
+    "lufthygiene_rosental",
+    default_args=default_args,
+    description="Run the lufthygiene_rosental docker container",
+    schedule_interval="*/30 * * * *",
+    catchup=False,
+) as dag:
     dag.doc_md = __doc__
     upload = DockerOperator(
-        task_id='upload',
-        image='lufthygiene_rosental:latest',
-        api_version='auto',
-        auto_remove='force',
-        command='python3 -m lufthygiene_rosental.etl',
-        container_name='lufthygiene_rosental',
+        task_id="upload",
+        image="ghcr.io/opendatabs/data-processing/lufthygiene_rosental:latest",
+        force_pull=True,
+        api_version="auto",
+        auto_remove="force",
+        command="uv run -m etl",
+        private_environment={
+            **COMMON_ENV_VARS,
+            "FTP_USER_07": Variable.get("FTP_USER_07"),
+            "FTP_PASS_07": Variable.get("FTP_PASS_07"),
+            "ODS_PUSH_URL_100275": Variable.get("ODS_PUSH_URL_100275"),
+        },
+        container_name="lufthygiene_rosental",
         docker_url="unix://var/run/docker.sock",
         network_mode="bridge",
         tty=True,
-        mounts=[Mount(source="/data/dev/workspace/data-processing", target="/code/data-processing", type="bind")]
+        mounts=[
+            Mount(
+                source=f"{PATH_TO_CODE}/data-processing/lufthygiene_rosental/data",
+                target="/code/data",
+                type="bind",
+            )
+        ],
     )
