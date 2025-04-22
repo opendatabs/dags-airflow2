c915a813fb710e4df7daf602f2a566bb63d721b3
Orhan Saeedi <orhan.saeedi@bs.ch>
2025-04-22 18:20:29 +0200

    Refactor iwb_gas

--- iwb_gas.py
+++ iwb_gas.py
@@ -6,51 +6,82 @@ This DAG updates the following datasets:
 - [100353](https://data.bs.ch/explore/dataset/100353)
 
 """
-from airflow import DAG
+
 from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.models import Variable
 from airflow.providers.docker.operators.docker import DockerOperator
 from docker.types import Mount
 
+from common_variables import COMMON_ENV_VARS, PATH_TO_CODE
+
 default_args = {
-    'owner': 'orhan.saeedi',
-    'description': 'Run the iwb_gas.py docker container',
-    'depend_on_past': False,
-    'start_date': datetime(2024, 1, 26),
-    'email': ["jonas.bieri@bs.ch", "orhan.saeedi@bs.ch", "rstam.aloush@bs.ch", "renato.farruggio@bs.ch"],
-    'email_on_failure': True,
-    'email_on_retry': False,
-    'retries': 0,
-    'retry_delay': timedelta(minutes=15)
+    "owner": "orhan.saeedi",
+    "depend_on_past": False,
+    "start_date": datetime(2024, 1, 26),
+    "email": Variable.get("EMAIL_RECEIVERS"),
+    "email_on_failure": True,
+    "email_on_retry": False,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=15),
 }
 
-with DAG('iwb_gas', default_args=default_args, schedule_interval="0 13 * * *", catchup=False) as dag:
+with DAG(
+    "iwb_gas",
+    default_args=default_args,
+    description="Run the iwb_gas docker container",
+    schedule_interval="0 13 * * *",
+    catchup=False,
+) as dag:
     dag.doc_md = __doc__
     upload = DockerOperator(
-        task_id='upload',
-        image='iwb_gas:latest',
-        api_version='auto',
-        auto_remove='force',
-        command='python3 -m iwb_gas.etl',
-        container_name='iwb_gas--upload',
+        task_id="upload",
+        image="ghcr.io/opendatabs/data-processing/iwb_gas:latest",
+        force_pull=True,
+        api_version="auto",
+        auto_remove="force",
+        command="uv run -m etl",
+        private_environment={
+            **COMMON_ENV_VARS,
+            "FTP_USER_04": Variable.get("FTP_USER_04"),
+            "FTP_PASS_04": Variable.get("FTP_PASS_04"),
+        },
+        container_name="iwb_gas--upload",
         docker_url="unix://var/run/docker.sock",
         network_mode="bridge",
         tty=True,
-        mounts=[Mount(source="/data/dev/workspace/data-processing", target="/code/data-processing", type="bind")]
+        mounts=[
+            Mount(
+                source=f"{PATH_TO_CODE}/data-processing/iwb_gas/data",
+                target="/code/data",
+                type="bind",
+            )
+        ],
     )
 
     fit_model = DockerOperator(
-        task_id='fit_model',
-        image='gasverbrauch:latest',
-        api_version='auto',
-        auto_remove='force',
-        command='Rscript /code/data-processing/stata_erwarteter_gasverbrauch/Gasverbrauch_OGD.R',
-        container_name='gasverbrauch--fit_model',
+        task_id="fit_model",
+        image="gasverbrauch:latest",
+        api_version="auto",
+        auto_remove="force",
+        command="Rscript /code/data-processing/stata_erwarteter_gasverbrauch/Gasverbrauch_OGD.R",
+        container_name="gasverbrauch--fit_model",
         docker_url="unix://var/run/docker.sock",
         network_mode="bridge",
         tty=True,
-        mounts=[Mount(source="/data/dev/workspace/data-processing", target="/code/data-processing", type="bind"),
-                Mount(source="/mnt/OGD-DataExch/StatA/Gasverbrauch",
-                      target="/code/data-processing/stata_erwarteter_gasverbrauch/data/export", type="bind")]
+        mounts=[
+            Mount(
+                source=f"{PATH_TO_CODE}/data-processing",
+                target="/code/data-processing",
+                type="bind",
+            ),
+            Mount(
+                source="/mnt/OGD-DataExch/StatA/Gasverbrauch",
+                target="/code/data-processing/stata_erwarteter_gasverbrauch/data/export",
+                type="bind",
+            ),
+        ],
     )
 
     upload >> fit_model
