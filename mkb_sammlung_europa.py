65d36cce1cf880fbfbaf2a1c124a627786c58caa
Orhan Saeedi <orhan.saeedi@bs.ch>
2025-04-24 11:50:09 +0200

    Refactor mkb_sammlung_europa

--- mkb_sammlung_europa.py
+++ mkb_sammlung_europa.py
@@ -6,36 +6,56 @@ This DAG updates the following datasets:
 
 """
 
-from airflow import DAG
 from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.models import Variable
 from airflow.providers.docker.operators.docker import DockerOperator
 from docker.types import Mount
 
+from common_variables import COMMON_ENV_VARS, PATH_TO_CODE
+
 default_args = {
-    'owner': 'hester.pieters',
-    'description': 'Run the mkb_sammlung_europa.py docker container',
-    'depend_on_past': False,
-    'start_date': datetime(2024, 2, 2),
-    'email': ["jonas.bieri@bs.ch", "orhan.saeedi@bs.ch", "rstam.aloush@bs.ch", "renato.farruggio@bs.ch"],
-    'email_on_failure': True,
-    'email_on_retry': False,
-    'retries': 0,
-    'retry_delay': timedelta(minutes=15)
+    "owner": "hester.pieters",
+    "depend_on_past": False,
+    "start_date": datetime(2024, 2, 2),
+    "email": Variable.get("EMAIL_RECEIVERS"),
+    "email_on_failure": True,
+    "email_on_retry": False,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=15),
 }
 
-with DAG('mkb_sammlung_europa', default_args=default_args, schedule_interval=None, catchup=False) as dag:
+with DAG(
+    "mkb_sammlung_europa",
+    default_args=default_args,
+    description="Run the mkb_sammlung_europa docker container",
+    schedule_interval=None,
+    catchup=False,
+) as dag:
     dag.doc_md = __doc__
     upload = DockerOperator(
-        task_id='upload',
-        image='mkb_sammlung_europa:latest',
-        api_version='auto',
-        auto_remove='force',
-        command='python3 -m mkb_sammlung_europa.etl',
-        container_name='mkb_sammlung_europa--upload',
+        task_id="upload",
+        image="ghcr.io/opendatabs/data-processing/mkb_sammlung_europa:latest",
+        force_pull=True,
+        api_version="auto",
+        auto_remove="force",
+        command="uv run -m etl",
+        private_environment=COMMON_ENV_VARS,
+        container_name="mkb_sammlung_europa--upload",
         docker_url="unix://var/run/docker.sock",
         network_mode="bridge",
         tty=True,
-        mounts=[Mount(source="/data/dev/workspace/data-processing", target="/code/data-processing", type="bind"),
-                Mount(source="/mnt/OGD-DataExch/PD-Kultur-MKB",
-                      target="/code/data-processing/mkb_sammlung_europa/data", type="bind")]
+        mounts=[
+            Mount(
+                source=f"{PATH_TO_CODE}/data-processing/mkb_sammlung_europa/change_tracking",
+                target="/code/change_tracking",
+                type="bind",
+            ),
+            Mount(
+                source="/mnt/OGD-DataExch/PD-Kultur-MKB",
+                target="/code/data",
+                type="bind",
+            ),
+        ],
     )
