3e00c2e6b10cc4ae5e89446dfaf11818ce1e7eaf
Orhan Saeedi <orhan.saeedi@bs.ch>
2025-04-24 15:07:34 +0200

    Refactor mobilitaet_mikromobilitaet

--- mobilitaet_mikromobilitaet.py
+++ mobilitaet_mikromobilitaet.py
@@ -5,36 +5,46 @@ This DAG updates the following datasets:
 - [100415](https://data.bs.ch/explore/dataset/100415)
 """
 
-from airflow import DAG
 from datetime import datetime, timedelta
-from airflow.models.dagrun import DagRun
-from airflow.providers.docker.operators.docker import DockerOperator
-from airflow.operators.python_operator import PythonOperator
-from docker.types import Mount
+
+from airflow import DAG
 from airflow.models import Variable
+from airflow.models.dagrun import DagRun
+from airflow.operators.python import PythonOperator
+from airflow.providers.docker.operators.docker import DockerOperator
+from docker.types import Mount
+
+from common_variables import COMMON_ENV_VARS, PATH_TO_CODE
+
 
 def check_manual_triggering(**context):
-    dag_run: DagRun = context.get('dag_run')
+    dag_run: DagRun = context.get("dag_run")
     # Below condition will return true if DAG is triggered manually.
     if dag_run.external_trigger:
         raise RuntimeError(
-            'Manual DAG run disallowed since it is only meant to be triggered by the scheduler. '
-            'For further info see job `mobilitaet_mikromobilitaet_stats` in the data-processing repository.'
+            "Manual DAG run disallowed since it is only meant to be triggered by the scheduler. "
+            "For further info see job `mobilitaet_mikromobilitaet_stats` in the data-processing repository."
         )
 
+
 default_args = {
-    'owner': 'orhan.saeedi',
-    'description': 'Run the mobilitaet_mikromobilitaet docker container',
-    'depend_on_past': False,
-    'start_date': datetime(2025, 1, 31),
-    'email': ["jonas.bieri@bs.ch", "orhan.saeedi@bs.ch", "rstam.aloush@bs.ch", "renato.farruggio@bs.ch"],
-    'email_on_failure': True,
-    'email_on_retry': False,
-    'retries': 0,
-    'retry_delay': timedelta(minutes=3)
+    "owner": "orhan.saeedi",
+    "depend_on_past": False,
+    "start_date": datetime(2025, 1, 31),
+    "email": Variable.get("EMAIL_RECEIVERS"),
+    "email_on_failure": True,
+    "email_on_retry": False,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=3),
 }
 
-with DAG('mobilitaet_mikromobilitaet', default_args=default_args, schedule_interval="*/10 * * * *", catchup=False) as dag:
+with DAG(
+    "mobilitaet_mikromobilitaet",
+    description="Run the mobilitaet_mikromobilitaet docker container",
+    default_args=default_args,
+    schedule_interval="*/10 * * * *",
+    catchup=False,
+) as dag:
     dag.doc_md = __doc__
 
     manual_trigger_check = PythonOperator(
@@ -43,20 +53,29 @@ with DAG('mobilitaet_mikromobilitaet', default_args=default_args, schedule_inter
     )
 
     process_upload = DockerOperator(
-        task_id='process-upload',
-        image='ghcr.io/opendatabs/data-processing/mobilitaet_mikromobilitaet:latest',
+        task_id="process-upload",
+        image="ghcr.io/opendatabs/data-processing/mobilitaet_mikromobilitaet:latest",
         force_pull=True,
-        api_version='auto',
-        auto_remove='force',
-        command='uv run -m src.etl',
-        container_name='mobilitaet_mikromobilitaet',
+        api_version="auto",
+        auto_remove="force",
+        command="uv run -m etl",
+        private_environment=COMMON_ENV_VARS,
+        container_name="mobilitaet_mikromobilitaet",
         docker_url="unix://var/run/docker.sock",
         network_mode="bridge",
         tty=True,
-        mounts=[Mount(source="/data/dev/workspace/data-processing", target="/code/data-processing", type="bind"),
-                Mount(source="/mnt/OGD-DataExch/StatA/BVD-MOB/Mikromobilitaet",
-                      target="/code/data-processing/mobilitaet_mikromobilitaet/data", type="bind")
-                ]
+        mounts=[
+            Mount(
+                source="/mnt/OGD-DataExch/StatA/BVD-MOB/Mikromobilitaet",
+                target="/code/data",
+                type="bind",
+            ),
+            Mount(
+                source=f"{PATH_TO_CODE}/data-processing/mobilitaet_mikromobilitaet/change_tracking",
+                target="/code/change_tracking",
+                type="bind",
+            ),
+        ],
     )
 
     manual_trigger_check >> process_upload
--- mobilitaet_mikromobilitaet_stats.py
+++ mobilitaet_mikromobilitaet_stats.py
@@ -7,43 +7,57 @@ This DAG updates the following datasets:
 - [100422](https://data.bs.ch/explore/dataset/100422)
 """
 
-from airflow import DAG
 from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.models import Variable
 from airflow.providers.docker.operators.docker import DockerOperator
 from docker.types import Mount
-from airflow.models import Variable
 
-# This is set in the Airflow UI under Admin -> Variables
-https_proxy = Variable.get("https_proxy")
+from common_variables import COMMON_ENV_VARS, PATH_TO_CODE
+
 
 default_args = {
-    'owner': 'orhan.saeedi',
-    'description': 'Run the mobilitaet_mikromobilitaet_stats docker container',
-    'depend_on_past': False,
-    'start_date': datetime(2025, 2, 12),
-    'email': ["jonas.bieri@bs.ch", "orhan.saeedi@bs.ch", "rstam.aloush@bs.ch", "renato.farruggio@bs.ch"],
-    'email_on_failure': True,
-    'email_on_retry': False,
-    'retries': 0,
-    'retry_delay': timedelta(minutes=3)
+    "owner": "orhan.saeedi",
+    "depend_on_past": False,
+    "start_date": datetime(2025, 2, 12),
+    "email": Variable.get("EMAIL_RECEIVERS"),
+    "email_on_failure": True,
+    "email_on_retry": False,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=3),
 }
 
-with DAG('mobilitaet_mikromobilitaet_stats', default_args=default_args, schedule_interval="0 4 * * *", catchup=False) as dag:
+with DAG(
+    "mobilitaet_mikromobilitaet_stats",
+    description="Run the mobilitaet_mikromobilitaet_stats",
+    default_args=default_args,
+    schedule_interval="0 4 * * *",
+    catchup=False,
+) as dag:
     dag.doc_md = __doc__
     process_upload = DockerOperator(
-        task_id='process-upload',
-        image='ghcr.io/opendatabs/data-processing/mobilitaet_mikromobilitaet_stats:latest',
+        task_id="process-upload",
+        image="ghcr.io/opendatabs/data-processing/mobilitaet_mikromobilitaet_stats:latest",
         force_pull=True,
-        api_version='auto',
-        auto_remove='force',
-        environment={'https_proxy': https_proxy},
-        command='uv run -m etl',
-        container_name='mobilitaet_mikromobilitaet_stats',
+        api_version="auto",
+        auto_remove="force",
+        command="uv run -m etl",
+        private_environment=COMMON_ENV_VARS,
+        container_name="mobilitaet_mikromobilitaet_stats",
         docker_url="unix://var/run/docker.sock",
         network_mode="bridge",
         tty=True,
-        mounts=[Mount(source="/data/dev/workspace/data-processing", target="/code/data-processing", type="bind"),
-                Mount(source="/mnt/OGD-DataExch/StatA/BVD-MOB/Mikromobilitaet",
-                      target="/code/data-processing/mobilitaet_mikromobilitaet_stats/data", type="bind")
-                ]
+        mounts=[
+            Mount(
+                source="/mnt/OGD-DataExch/StatA/BVD-MOB/Mikromobilitaet",
+                target="/code/data",
+                type="bind",
+            ),
+            Mount(
+                source=f"{PATH_TO_CODE}/data-processing/mobilitaet_mikromobilitaet_stats/change_tracking",
+                target="/code/change_tracking",
+                type="bind",
+            ),
+        ],
     )
