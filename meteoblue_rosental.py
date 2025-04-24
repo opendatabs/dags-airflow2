91bb96c7d2cd0d52516e0b1781dcfa0bc6977b58
Orhan Saeedi <orhan.saeedi@bs.ch>
2025-04-24 11:16:28 +0200

    Refactor meteoblue_rosental

--- meteoblue_rosental.py
+++ meteoblue_rosental.py
@@ -4,40 +4,59 @@ This DAG updates the following dataset:
 
 - [100294](https://data.bs.ch/explore/dataset/100294)
 """
-from airflow import DAG
+
 from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.models import Variable
 from airflow.providers.docker.operators.docker import DockerOperator
 from docker.types import Mount
-from airflow.models import Variable
 
-# This is set in the Airflow UI under Admin -> Variables
-https_proxy = Variable.get("https_proxy")
+from common_variables import COMMON_ENV_VARS, PATH_TO_CODE
+
 
 default_args = {
-    'owner': 'hester.pieters',
-    'description': 'Run the meteoblue_rosental docker container',
-    'depend_on_past': False,
-    'start_date': datetime(2024, 1, 26),
-    'email': ["jonas.bieri@bs.ch", "orhan.saeedi@bs.ch", "rstam.aloush@bs.ch", "renato.farruggio@bs.ch"],
-    'email_on_failure': True,
-    'email_on_retry': False,
-    'retries': 0,
-    'retry_delay': timedelta(minutes=3)
+    "owner": "hester.pieters",
+    "description": "Run the meteoblue_rosental docker container",
+    "depend_on_past": False,
+    "start_date": datetime(2024, 1, 26),
+    "email": Variable.get("EMAIL_RECEIVERS"),
+    "email_on_failure": True,
+    "email_on_retry": False,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=3),
 }
 
-with DAG('meteoblue_rosental', default_args=default_args, schedule_interval="45 * * * *", catchup=False) as dag:
+with DAG(
+    "meteoblue_rosental",
+    default_args=default_args,
+    schedule_interval="45 * * * *",
+    catchup=False,
+) as dag:
     dag.doc_md = __doc__
     upload = DockerOperator(
-        task_id='upload',
-        image='ghcr.io/opendatabs/data-processing/meteoblue_rosental:latest',
+        task_id="upload",
+        image="ghcr.io/opendatabs/data-processing/meteoblue_rosental:latest",
         force_pull=True,
-        api_version='auto',
-        auto_remove='force',
-        environment={'https_proxy': https_proxy},
-        command='uv run -m etl',
-        container_name='meteoblue_rosental',
+        api_version="auto",
+        auto_remove="force",
+        private_environment={
+            **COMMON_ENV_VARS,
+            "API_KEY": Variable.get("API_KEY_METEOBLUE"),
+            "FTP_USER_07": Variable.get("FTP_USER_07"),
+            "FTP_PASS_07": Variable.get("FTP_PASS_07"),
+            "ODS_PUSH_URL_100294": Variable.get("ODS_PUSH_URL_100294"),
+        },
+        command="uv run -m etl",
+        container_name="meteoblue_rosental",
         docker_url="unix://var/run/docker.sock",
         network_mode="bridge",
         tty=True,
-        mounts=[Mount(source="/data/dev/workspace/data-processing", target="/code/data-processing", type="bind")]
+        mounts=[
+            Mount(
+                source=f"{PATH_TO_CODE}/data-processing/meteoblue_rosental/data",
+                target="/code/data",
+                type="bind",
+            )
+        ],
     )
