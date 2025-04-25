e2752367cf6f430c2693c6fa073d388ecd46309d
Orhan Saeedi <orhan.saeedi@bs.ch>
2025-04-25 10:31:19 +0200

    Refactor parkendd

--- parkendd.py
+++ parkendd.py
@@ -6,34 +6,56 @@ This DAG updates the following datasets:
 - [100044](https://data.bs.ch/explore/dataset/100044)
 """
 
-from airflow import DAG
 from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.models import Variable
 from airflow.providers.docker.operators.docker import DockerOperator
 from docker.types import Mount
 
+from common_variables import COMMON_ENV_VARS, PATH_TO_CODE
+
 default_args = {
-    'owner': 'jonas.bieri',
-    'description': 'Run the parkendd docker container',
-    'depend_on_past': False,
-    'start_date': datetime(2024, 2, 2),
-    'email': ["jonas.bieri@bs.ch", "orhan.saeedi@bs.ch", "rstam.aloush@bs.ch", "renato.farruggio@bs.ch"],
-    'email_on_failure': True,
-    'email_on_retry': False,
-    'retries': 0,
-    'retry_delay': timedelta(minutes=3)
+    "owner": "jonas.bieri",
+    "description": "Run the parkendd docker container",
+    "depend_on_past": False,
+    "start_date": datetime(2024, 2, 2),
+    "email": Variable.get("EMAIL_RECEIVERS"),
+    "email_on_failure": True,
+    "email_on_retry": False,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=3),
 }
 
-with DAG('parkendd', default_args=default_args, schedule_interval="0 * * * *", catchup=False) as dag:
+with DAG(
+    "parkendd",
+    description="Run the parkendd docker container",
+    default_args=default_args,
+    schedule_interval="0 * * * *",
+    catchup=False,
+) as dag:
     dag.doc_md = __doc__
     process_upload = DockerOperator(
-        task_id='process-upload',
-        image='parkendd:latest',
-        api_version='auto',
-        auto_remove='force',
-        command='python3 -m parkendd.etl',
-        container_name='parkendd',
+        task_id="process-upload",
+        image="ghcr.io/opendatabs/data-processing/parkendd:latest",
+        api_version="auto",
+        auto_remove="force",
+        command="uv run -m etl",
+        private_environment=COMMON_ENV_VARS,
+        container_name="parkendd",
         docker_url="unix://var/run/docker.sock",
         network_mode="bridge",
         tty=True,
-        mounts=[Mount(source="/data/dev/workspace/data-processing", target="/code/data-processing", type="bind")]
+        mounts=[
+            Mount(
+                source=f"{PATH_TO_CODE}/data-processing/parkendd/data",
+                target="/code/data",
+                type="bind",
+            ),
+            Mount(
+                source=f"{PATH_TO_CODE}/data-processing/parkendd/change_tracking",
+                target="/code/change_tracking",
+                type="bind",
+            ),
+        ],
     )
