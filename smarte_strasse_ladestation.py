412823cf6eed1a28037153fd8d65050e6c998a7b
Orhan Saeedi <orhan.saeedi@bs.ch>
2025-04-26 08:56:11 +0200

    Refactor smarte_strasse_ladestation

--- smarte_strasse_ladestation.py
+++ smarte_strasse_ladestation.py
@@ -5,35 +5,51 @@ This DAG updates the following datasets:
 - [100047](https://data.bs.ch/explore/dataset/100047)
 """
 
-from airflow import DAG
 from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.models import Variable
 from airflow.providers.docker.operators.docker import DockerOperator
-from docker.types import Mount
+
+from common_variables import COMMON_ENV_VARS
 
 default_args = {
-    'owner': 'jonas.bieri',
-    'description': 'Run the smarte_strasse_ladestation docker container',
-    'depend_on_past': False,
-    'start_date': datetime(2024, 2, 2),
-    'email': ["jonas.bieri@bs.ch", "orhan.saeedi@bs.ch", "rstam.aloush@bs.ch", "renato.farruggio@bs.ch"],
-    'email_on_failure': True,
-    'email_on_retry': False,
-    'retries': 0,
-    'retry_delay': timedelta(minutes=15)
+    "owner": "jonas.bieri",
+    "depend_on_past": False,
+    "start_date": datetime(2024, 2, 2),
+    "email": Variable.get("EMAIL_RECEIVERS"),
+    "email_on_failure": True,
+    "email_on_retry": False,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=15),
 }
 
-with DAG('smarte_strasse_ladestation', default_args=default_args, schedule_interval="*/15 * * * *",
-         catchup=False) as dag:
+with DAG(
+    "smarte_strasse_ladestation",
+    description="Run the smarte_strasse_ladestation docker container",
+    default_args=default_args,
+    schedule_interval="*/15 * * * *",
+    catchup=False,
+) as dag:
     dag.doc_md = __doc__
     upload = DockerOperator(
-        task_id='upload',
-        image='smarte_strasse_ladestation:latest',
-        api_version='auto',
-        auto_remove='force',
-        command='python3 -m smarte_strasse_ladestation.etl',
-        container_name='smarte_strasse_ladestation',
+        task_id="upload",
+        image="ghcr.io/opendatabs/data-processing/smarte_strasse_ladestation:latest",
+        force_pull=True,
+        api_version="auto",
+        auto_remove="force",
+        command="uv run -m etl",
+        private_environment={
+            **COMMON_ENV_VARS,
+            "ODS_PUSH_URL": Variable.get("ODS_PUSH_URL"),
+            "HTTPS_URL": Variable.get("HTTPS_URL_LADESTATIONEN"),
+            "HTTPS_URL_AUTH": Variable.get("HTTPS_URL_AUTH_LADESTATIONEN"),
+            "HTTPS_USER": Variable.get("HTTPS_USER_LADESTATIONEN"),
+            "HTTPS_PASS": Variable.get("HTTPS_PASS_LADESTATIONEN"),
+            "API_KEY": Variable.get("API_KEY_LADESTATIONEN"),
+        },
+        container_name="smarte_strasse_ladestation",
         docker_url="unix://var/run/docker.sock",
         network_mode="bridge",
         tty=True,
-        mounts=[Mount(source="/data/dev/workspace/data-processing", target="/code/data-processing", type="bind")]
     )
