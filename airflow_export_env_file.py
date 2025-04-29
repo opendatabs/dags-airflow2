48d1c0dbae0fad77c1c9dd90c9c99634e6f697c4
Orhan Saeedi <orhan.saeedi@bs.ch>
2025-04-29 17:53:57 +0200

    Try writing from database

--- airflow_export_env_file.py
+++ airflow_export_env_file.py
@@ -1,6 +1,6 @@
 """
-# airflow_export_env_file
-This DAG exports the variables to an env file to run jobs locally
+# airflow_export_variables
+This DAG exports all Airflow Variables into a .env file inside a mounted directory
 """
 
 from datetime import datetime, timedelta
@@ -8,49 +8,55 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.models import Variable
 from airflow.providers.docker.operators.docker import DockerOperator
-from datetime import datetime
+from airflow.settings import Session
 from docker.types import Mount
 
-# Fetch all variables
-all_vars = Variable.get_variable_from_secrets()
-env_vars = {k: v for k, v in all_vars.items() if not k.startswith('PATH_TO_')}
+# Fetch path to mount from Airflow Variables
+PATH_TO_CREDENTIALS = Variable.get("PATH_TO_CREDENTIALS")
 
+# Open DB session to fetch all Variables
+session = Session()
+# Fetch all variables and EXCLUDE those starting with 'PATH_TO'
+all_vars = {var.key: var.val for var in session.query(Variable).all() if not var.key.startswith("PATH_TO")}
+session.close()
+
+# Define default arguments for the DAG
 default_args = {
     "owner": "orhan.saeedi",
-    "depend_on_past": False,
-    "start_date": datetime(2025, 2, 26),
-    "email": Variable.get("EMAIL_RECEIVERS"),
+    "description": "Export Airflow Variables to a .env file",
+    "depends_on_past": False,
+    "start_date": datetime(2024, 10, 11),
+    "retries": 0,
+    "retry_delay": timedelta(minutes=15),
     "email_on_failure": True,
     "email_on_retry": False,
-    "retries": 0,
-    "retry_delay": timedelta(minutes=3),
+    "email": Variable.get("EMAIL_RECEIVERS"),
 }
 
-
+# Define the DAG
 with DAG(
-    dag_id="export_all_airflow_vars_to_env",
+    "airflow_export_variables",
     default_args=default_args,
+    description="DockerOperator to export Airflow Variables into a .env file",
     schedule_interval=None,
     catchup=False,
 ) as dag:
 
-    export_env = DockerOperator(
-        task_id="export_env_file",
-        image="python:3.12-slim",
-        environment=env_vars,
-        command="bash -c 'printenv > /code/.env && cat /code/.env'",
+    export_env_vars = DockerOperator(
+        task_id="export_variables_to_env",
+        image="apache/airflow:2.7.2",
+        container_name="airflow_export_env_container",
+        api_version="auto",
         auto_remove="force",
-        mount_tmp_dir=False,
-        container_name="export_env_file",
+        command="bash -c \"mkdir -p /opt/airflow/credentials && printenv | sort > /opt/airflow/credentials/.env && cat /opt/airflow/credentials/.env\"",
         docker_url="unix://var/run/docker.sock",
         network_mode="airflow_default",
-        tty=True,
-        do_xcom_push=False,
+        environment=all_vars,
         mounts=[
             Mount(
-                source=Variable.get("PATH_TO_CREDENTIALS"),
-                target="/code",
+                source=PATH_TO_CREDENTIALS,
+                target="/opt/airflow/credentials",
                 type="bind",
-            )
-        ]
+            ),
+        ],
     )
