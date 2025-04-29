53b7b517f599fd64ea8178ef8d839e7d804d24f0
Orhan Saeedi <orhan.saeedi@bs.ch>
2025-04-29 17:59:01 +0200

    Run as root

--- airflow_export_env_file.py
+++ airflow_export_env_file.py
@@ -48,6 +48,7 @@ with DAG(
         container_name="airflow_export_env_container",
         api_version="auto",
         auto_remove="force",
+        user="0",
         command="bash -c \"mkdir -p /opt/airflow/credentials && printenv | sort > /opt/airflow/credentials/.env && cat /opt/airflow/credentials/.env\"",
         docker_url="unix://var/run/docker.sock",
         network_mode="airflow_default",
