bf1d44c227aabc0833a6335bd5b925ac65c2b8ff
Orhan Saeedi <orhan.saeedi@bs.ch>
2025-05-19 15:09:55 +0200

    Add DAG script for updating datasets related to KI FAQ

---
+++ dcc_ki_faq.py
@@ -0,0 +1,65 @@
+"""
+# dcc_ki_faq.py
+This DAG updates the following datasets:
+
+- [100443](https://data.bs.ch/explore/dataset/100443)
+"""
+
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.models import Variable
+from airflow.providers.docker.operators.docker import DockerOperator
+from docker.types import Mount
+
+from common_variables import COMMON_ENV_VARS, PATH_TO_CODE
+
+default_args = {
+    "owner": "sarah.carter",
+    "depend_on_past": False,
+    "start_date": datetime(2025, 5, 19),
+    "email": Variable.get("EMAIL_RECEIVERS"),
+    "email_on_failure": True,
+    "email_on_retry": False,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=15),
+}
+
+with DAG(
+    "dcc_ki_faq",
+    default_args=default_args,
+    description="Run the dcc_ki_faq docker container",
+    schedule_interval="*/5 * * * *",
+    catchup=False,
+) as dag:
+    dag.doc_md = __doc__
+    upload = DockerOperator(
+        task_id="upload",
+        image="ghcr.io/opendatabs/data-processing/dcc_ki_faq:latest",
+        force_pull=True,
+        api_version="auto",
+        auto_remove="force",
+        command="uv run -m etl",
+        private_environment=COMMON_ENV_VARS,
+        container_name="dcc_ki_faq",
+        docker_url="unix://var/run/docker.sock",
+        network_mode="bridge",
+        tty=True,
+        mounts=[
+            Mount(
+                source=f"{PATH_TO_CODE}/data-processing/dcc_ki_faq/data",
+                target="/code/data",
+                type="bind",
+            ),
+            Mount(
+                source="/mnt/OGD-DataExch/StatA/KI-FAQ",
+                target="/code/data_orig",
+                type="bind",
+            ),
+            Mount(
+                source=f"{PATH_TO_CODE}/data-processing/dcc_ki_faq/change_tracking",
+                target="/code/change_tracking",
+                type="bind",
+            ),
+        ],
+    )
