20bb108491bbf2184ddb6a8f4ddf57f138cb473e
Orhan Saeedi <orhan.saeedi@bs.ch>
2025-05-27 13:39:31 +0200

    Add awa_feiertage

---
+++ awa_feiertage.py
@@ -0,0 +1,65 @@
+"""
+# awa_feiertage
+This DAG updates the following datasets:
+
+- [100448](https://data.bs.ch/explore/dataset/100448)
+"""
+
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.models import Variable
+from airflow.providers.docker.operators.docker import DockerOperator
+from docker.types import Mount
+
+from common_variables import COMMON_ENV_VARS, PATH_TO_CODE
+
+default_args = {
+    "owner": "orhan.saeedi",
+    "depend_on_past": False,
+    "start_date": datetime(2025, 5, 26),
+    "email": Variable.get("EMAIL_RECEIVERS"),
+    "email_on_failure": True,
+    "email_on_retry": False,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=15),
+}
+
+with DAG(
+    "awa_feiertage",
+    description="Run the awa_feiertage docker container",
+    default_args=default_args,
+    schedule_interval="49 14 * * *",
+    catchup=False,
+) as dag:
+    dag.doc_md = __doc__
+    upload_bag_datasets = DockerOperator(
+        task_id="upload",
+        image="ghcr.io/opendatabs/data-processing/awa_feiertage:latest",
+        force_pull=True,
+        api_version="auto",
+        auto_remove="force",
+        command="uv run -m etl",
+        private_environment=COMMON_ENV_VARS,
+        container_name="awa_feiertage--upload",
+        docker_url="unix://var/run/docker.sock",
+        network_mode="bridge",
+        tty=True,
+        mounts=[
+            Mount(
+                source="/mnt/OGD-DataExch/StatA/FreiUndFeiertage",
+                target="/code/data_orig",
+                type="bind",
+            ),
+            Mount(
+                source=f"{PATH_TO_CODE}/data_processing/awa_feiertage/data",
+                target="/code/data",
+                type="bind",
+            ),
+            Mount(
+                source=f"{PATH_TO_CODE}/data_processing/awa_feiertage/change_tracking",
+                target="/code/change_tracking",
+                type="bind",
+            ),
+        ],
+    )
