f25bbe29e68c0176f6fe36e78117b59180dd6f0c
Orhan Saeedi <orhan.saeedi@bs.ch>
2025-06-02 17:36:04 +0200

    Update staka_regierungsratsbeschluesse.py

--- staka_regierungsratsbeschluesse.py
+++ staka_regierungsratsbeschluesse.py
@@ -42,7 +42,7 @@ with DAG(
         force_pull=True,
         api_version="auto",
         auto_remove="force",
-        command="uv run -m src.etl",
+        command="uv run -m etl",
         private_environment=COMMON_ENV_VARS,
         container_name="staka_regierungsratsbeschluesse",
         docker_url="unix://var/run/docker.sock",
