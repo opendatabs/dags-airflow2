14cc6196daba222de4f263af30b8246350ba328a
Orhan Saeedi <orhan.saeedi@bs.ch>
2025-08-21 09:55:54 +0200

    Update DAG schedule to run on Sundays at 5 AM

--- parlamentsdienst_grosserrat_datasette.py
+++ parlamentsdienst_grosserrat_datasette.py
@@ -26,7 +26,7 @@ with DAG(
     "parlamentsdienst_grosserrat_datasette",
     default_args=default_args,
     description="Run the parlamentsdienst_grosserrat_datasette docker container",
-    schedule="0 5 * * *",
+    schedule="0 5 * * 0",
     catchup=False,
 ) as dag:
     dag.doc_md = __doc__
