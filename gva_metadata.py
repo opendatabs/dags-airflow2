d3d09784417dc2bf0941a0918b82ff8d19db7af5
rstam-aloush <rstam.aloush@bs.ch>
2025-09-09 11:18:28 +0200

    Add gva_metadata DAG

---
+++ gva_metadata.py
@@ -0,0 +1,61 @@
+"""
+# gva_metadata
+This DAG updates the following datasets:
+
+- [100075](https://data.bs.ch/explore/dataset/100410)
+"""
+
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.models import Variable
+from airflow.providers.docker.operators.docker import DockerOperator
+from docker.types import Mount
+
+from common_variables import COMMON_ENV_VARS, PATH_TO_CODE
+
+default_args = {
+    "owner": "rstam.aloush",
+    "depend_on_past": False,
+    "start_date": datetime(2025, 9, 9),
+    "email": Variable.get("EMAIL_RECEIVERS"),
+    "email_on_failure": True,
+    "email_on_retry": False,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=15),
+}
+
+with DAG(
+    "gva_metadata",
+    default_args=default_args,
+    description="Run the gva_metadata docker container",
+    schedule="*0 * * * *",
+    catchup=False,
+) as dag:
+    dag.doc_md = __doc__
+    upload = DockerOperator(
+        task_id="upload",
+        image="ghcr.io/opendatabs/data-processing/gva_metadata:latest",
+        force_pull=True,
+        api_version="auto",
+        auto_remove="force",
+        mount_tmp_dir=False,
+        command="uv run -m etl",
+        private_environment=COMMON_ENV_VARS,
+        container_name="gva_metadata",
+        docker_url="unix://var/run/docker.sock",
+        network_mode="bridge",
+        tty=True,
+        mounts=[
+            Mount(
+                source=f"{PATH_TO_CODE}/data-processing/gva_metadata/data",
+                target="/code/data",
+                type="bind",
+            ),
+            Mount(
+                source=f"{PATH_TO_CODE}/data-processing/gva_metadata/change_tracking",
+                target="/code/change_tracking",
+                type="bind",
+            ),
+        ],
+    )
