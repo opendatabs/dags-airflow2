a12851b0498c9ec02e4bb9d8676b133140483b39
Orhan Saeedi <orhan.saeedi@bs.ch>
2025-09-17 11:38:35 +0200

    Set user to 'root' in DockerOperator configuration in iwb_gas.py

--- iwb_gas.py
+++ iwb_gas.py
@@ -70,6 +70,7 @@ with DAG(
         mount_tmp_dir=False,
         command="Rscript Gasverbrauch_OGD.R",
         private_environment=COMMON_ENV_VARS,
+        user="root",
         container_name="gasverbrauch--fit_model",
         docker_url="unix://var/run/docker.sock",
         network_mode="bridge",
