ab748940c61ae173bdad4bf47d77e0ac007b7b53
Orhan Saeedi <orhan.saeedi@bs.ch>
2025-09-18 16:59:09 +0200

    Change DockerOperator user configuration from 'rstudio' to '1000:100' in iwb_gas.py

--- iwb_gas.py
+++ iwb_gas.py
@@ -70,7 +70,7 @@ with DAG(
         mount_tmp_dir=False,
         command="Rscript Gasverbrauch_OGD.R",
         private_environment=COMMON_ENV_VARS,
-        user="rstudio",
+        user="1000:100",
         container_name="gasverbrauch--fit_model",
         docker_url="unix://var/run/docker.sock",
         network_mode="bridge",
