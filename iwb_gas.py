102858c049307f314010fbac6481803f207eabfe
Orhan Saeedi <orhan.saeedi@bs.ch>
2025-09-18 16:34:53 +0200

    Remove 'user' configuration set to 'root' in DockerOperator across multiple DAG files

--- iwb_gas.py
+++ iwb_gas.py
@@ -70,7 +70,6 @@ with DAG(
         mount_tmp_dir=False,
         command="Rscript Gasverbrauch_OGD.R",
         private_environment=COMMON_ENV_VARS,
-        user="root",
         container_name="gasverbrauch--fit_model",
         docker_url="unix://var/run/docker.sock",
         network_mode="bridge",
