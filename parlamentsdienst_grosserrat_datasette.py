8912e326fe928377f55692b91034bf065630d01d
Orhan Saeedi <orhan.saeedi@bs.ch>
2025-11-14 16:38:12 +0100

    Disable email notifications on failure for parlamentsdienst_grosserrat_datasette DAG

--- parlamentsdienst_grosserrat_datasette.py
+++ parlamentsdienst_grosserrat_datasette.py
@@ -20,7 +20,7 @@ default_args = {
     "depend_on_past": False,
     "start_date": datetime(2025, 5, 8),
     "email": Variable.get("EMAIL_RECEIVERS"),
-    "email_on_failure": True,
+    "email_on_failure": False,
     "email_on_retry": False,
     "retries": 0,
     "retry_delay": timedelta(minutes=15),
