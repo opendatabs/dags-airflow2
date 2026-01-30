2e0dcaf520c56845f67ad7ec1e1809cf30057deb
Orhan Saeedi <orhan.saeedi@bs.ch>
2026-01-30 11:52:22 +0100

    Add staka_verz_verf_persdat

---
+++ staka_verz_verf_persdat.py
@@ -0,0 +1,71 @@
+"""
+# staka_verz_verf_persdat.py
+This DAG helps populating the following datasets:
+
+[100520](https://data.bs.ch/explore/dataset/100520)
+"""
+
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.models import Variable
+from helpers.failure_tracking_operator import FailureTrackingDockerOperator
+from docker.types import Mount
+
+from common_variables import COMMON_ENV_VARS, PATH_TO_CODE
+
+# DAG configuration
+DAG_ID = "staka_verz_verf_persdat"
+FAILURE_THRESHOLD = 1
+EXECUTION_TIMEOUT = timedelta(minutes=60)
+SCHEDULE = "* */3 * * *"
+
+default_args = {
+    "owner": "orhan.saeedi",
+    "depend_on_past": False,
+    "start_date": datetime(2026, 1, 30),
+    "email": Variable.get("EMAIL_RECEIVERS"),
+    "email_on_failure": True,
+    "email_on_retry": False,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=5),
+}
+
+with DAG(
+    dag_id=DAG_ID,
+    description=f"Run the {DAG_ID} docker container",
+    default_args=default_args,
+    schedule=SCHEDULE,
+    catchup=False,
+) as dag:
+    dag.doc_md = __doc__
+
+    # Main task to run the script
+    upload = FailureTrackingDockerOperator(
+        task_id="upload",
+        failure_threshold=FAILURE_THRESHOLD,
+        execution_timeout=EXECUTION_TIMEOUT,
+        image=f"ghcr.io/opendatabs/data-processing/{DAG_ID}:latest",
+        force_pull=True,
+        api_version="auto",
+        auto_remove="force",
+        mount_tmp_dir=False,
+        command="uv run -m etl",
+        private_environment=COMMON_ENV_VARS,
+        container_name=DAG_ID,
+        docker_url="unix://var/run/docker.sock",
+        network_mode="bridge",
+        tty=True,
+        mounts=[
+            Mount(
+                source=f"{PATH_TO_CODE}/data-processing/{DAG_ID}/data",
+                target="/code/data",
+                type="bind",
+            ),
+            Mount(
+                source=f"{PATH_TO_CODE}/data-processing/{DAG_ID}/change_tracking",
+                target="/code/change_tracking",
+                type="bind",
+            ),
+        ],
+    )
