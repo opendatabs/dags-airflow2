b2985dbfaad66e4273fa8a27b0501c4dd3da37e0
Orhan Saeedi <orhan.saeedi@bs.ch>
2026-05-08 16:06:08 +0200

    Add new DAG for kantonslabor dataset processing with cleanup and upload tasks

---
+++ kantonslabor_coliminder.py
@@ -0,0 +1,83 @@
+"""
+# kantonslabor_coliminder.py
+This DAG helps populating the following datasets:
+
+[100530](https://data.bs.ch/explore/dataset/100530)
+"""
+
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.models import Variable
+from helpers.failure_tracking_operator import FailureTrackingDockerOperator
+from airflow.operators.bash import BashOperator
+from docker.types import Mount
+
+from common_variables import COMMON_ENV_VARS, PATH_TO_CODE
+
+# DAG configuration
+DAG_ID = "kantonslabor_coliminder"
+FAILURE_THRESHOLD = 1
+EXECUTION_TIMEOUT = timedelta(minutes=45)
+SCHEDULE = "53 * * * *"
+
+default_args = {
+    "owner": "orhan.saeedi",
+    "depend_on_past": False,
+    "start_date": datetime(2026, 5, 8),
+    "email": Variable.get("EMAIL_RECEIVERS"),
+    "email_on_failure": True,
+    "email_on_retry": False,
+    "retries": 0,
+    "retry_delay": timedelta(minutes=3),
+}
+
+with DAG(
+    dag_id=DAG_ID,
+    description=f"Run the {DAG_ID} docker container",
+    default_args=default_args,
+    schedule=SCHEDULE,
+    catchup=False,
+) as dag:
+    dag.doc_md = __doc__
+
+    # Cleanup task to remove any old containers at the beginning
+    cleanup_containers = BashOperator(
+        task_id="cleanup_old_containers",
+        bash_command=f'''
+            docker rm -f {DAG_ID} 2>/dev/null || true
+            ''',
+    )
+
+    # Main task to run the script
+    upload = FailureTrackingDockerOperator(
+        task_id="upload",
+        failure_threshold=FAILURE_THRESHOLD,
+        execution_timeout=EXECUTION_TIMEOUT,
+        image=f"ghcr.io/opendatabs/data-processing/{DAG_ID}:latest",
+        force_pull=True,
+        api_version="auto",
+        auto_remove="force",
+        mount_tmp_dir=False,
+        command="uv run -m etl",
+        private_environment=COMMON_ENV_VARS,
+        container_name=DAG_ID,
+        docker_url="unix://var/run/docker.sock",
+        network_mode="bridge",
+        tty=True,
+        mounts=[
+            Mount(
+                source=f"/mnt/OGD-DataExch/GD-Kantonslabor-Badewasserqualitaet",
+                target="/code/data",
+                type="bind",
+            ),
+            Mount(
+                source=f"{PATH_TO_CODE}/data-processing/{DAG_ID}/change_tracking",
+                target="/code/change_tracking",
+                type="bind",
+            ),
+        ],
+    )
+
+    # Set the task dependency
+    cleanup_containers >> upload
