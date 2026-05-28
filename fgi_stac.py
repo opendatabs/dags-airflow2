b2f23b3be66ec27b1e428d0ff029dd3c20449aeb
Orhan Saeedi <orhan.saeedi@bs.ch>
2026-05-28 08:17:21 +0200

    Change schedule

--- fgi_stac.py
+++ fgi_stac.py
@@ -24,8 +24,8 @@ from common_variables import COMMON_ENV_VARS, PATH_TO_CODE
 
 DAG_ID = "fgi_stac"
 FAILURE_THRESHOLD = 1  # Skip first failure, fail on second.
-EXECUTION_TIMEOUT = timedelta(minutes=1440)
-SCHEDULE = None
+EXECUTION_TIMEOUT = timedelta(minutes=55)
+SCHEDULE = "51 * * * *"
 
 TASK_IDS = ("sync_catalog", "prepare_assets", "publish")
 
