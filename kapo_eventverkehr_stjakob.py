477e49dd71b7bb4c91a1a6d9a41116107503753e
Orhan Saeedi <orhan.saeedi@bs.ch>
2026-06-09 15:55:58 +0200

    Disable email notifications on failure for kapo_eventverkehr_stjakob DAG

--- kapo_eventverkehr_stjakob.py
+++ kapo_eventverkehr_stjakob.py
@@ -28,7 +28,7 @@ default_args = {
     "depend_on_past": False,
     "start_date": datetime(2025, 1, 31),
     "email": Variable.get("EMAIL_RECEIVERS"),
-    "email_on_failure": True,
+    "email_on_failure": False,
     "email_on_retry": False,
     "retries": 0,
     "retry_delay": timedelta(minutes=15),
