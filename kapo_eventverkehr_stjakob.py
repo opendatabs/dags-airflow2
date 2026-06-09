f6e47cfe05deed69d80a5cbbb2e8fe73c4cc560f
Orhan Saeedi <orhan.saeedi@bs.ch>
2026-06-09 15:53:27 +0200

    Enhance kapo_eventverkehr_stjakob DAG with new dataset integration, extended execution timeout, and cleanup task using BashOperator

--- kapo_eventverkehr_stjakob.py
+++ kapo_eventverkehr_stjakob.py
@@ -1,24 +1,26 @@
 """
-# kapo_eventverkehr_stjakob.py
+# kapo_eventverkehr_stjakob
 This DAG updates the following datasets:
 
 - [100419](https://data.bs.ch/explore/dataset/100419)
 - [100429](https://data.bs.ch/explore/dataset/100429)
+- [100464](https://data.bs.ch/explore/dataset/100464)
 """
 
 from datetime import datetime, timedelta
 
 from airflow import DAG
 from airflow.models import Variable
+from helpers.failure_tracking_operator import FailureTrackingDockerOperator
+from airflow.operators.bash import BashOperator
 from docker.types import Mount
 
 from common_variables import COMMON_ENV_VARS, PATH_TO_CODE
-from helpers.failure_tracking_operator import FailureTrackingDockerOperator
 
 # DAG configuration
 DAG_ID = "kapo_eventverkehr_stjakob"
 FAILURE_THRESHOLD = 0  # Immediate failure with no skipping
-EXECUTION_TIMEOUT = timedelta(minutes=4)
+EXECUTION_TIMEOUT = timedelta(minutes=10)
 SCHEDULE = "*/5 * * * *"
 
 default_args = {
@@ -34,27 +36,45 @@ default_args = {
 
 with DAG(
     dag_id=DAG_ID,
-    default_args=default_args,
     description=f"Run the {DAG_ID} docker container",
-    schedule_interval=SCHEDULE,
+    default_args=default_args,
+    schedule=SCHEDULE,
     catchup=False,
 ) as dag:
     dag.doc_md = __doc__
+
+    cleanup_containers = BashOperator(
+        task_id="cleanup_old_containers",
+        bash_command=f'''
+            docker rm -f {DAG_ID} 2>/dev/null || true
+            ''',
+    )
+
     upload = FailureTrackingDockerOperator(
         task_id="upload",
+        failure_threshold=FAILURE_THRESHOLD,
+        execution_timeout=EXECUTION_TIMEOUT,
         image=f"ghcr.io/opendatabs/data-processing/{DAG_ID}:latest",
         force_pull=True,
         api_version="auto",
         auto_remove="force",
         mount_tmp_dir=False,
         command="uv run -m etl",
-        private_environment=COMMON_ENV_VARS,
+        private_environment={
+            **COMMON_ENV_VARS,
+            "SHAREPOINT_TENANT_ID": Variable.get("SHAREPOINT_TENANT_ID"),
+            "SHAREPOINT_CLIENT_ID": Variable.get("SHAREPOINT_CLIENT_ID"),
+            "SHAREPOINT_HOST": Variable.get("SHAREPOINT_HOST"),
+            "SHAREPOINT_SITE_NAME_KAPO_EVENTVERKEHR_STJAKOB": Variable.get(
+                "SHAREPOINT_SITE_NAME_KAPO_EVENTVERKEHR_STJAKOB"
+            ),
+            "SHAREPOINT_CERT_PATH": Variable.get("SHAREPOINT_CERT_PATH"),
+            "SHAREPOINT_THUMBPRINT": Variable.get("SHAREPOINT_THUMBPRINT"),
+        },
         container_name=DAG_ID,
         docker_url="unix://var/run/docker.sock",
         network_mode="bridge",
         tty=True,
-        failure_threshold=FAILURE_THRESHOLD,
-        execution_timeout=EXECUTION_TIMEOUT,
         mounts=[
             Mount(
                 source=f"{PATH_TO_CODE}/data-processing/{DAG_ID}/data",
@@ -62,7 +82,7 @@ with DAG(
                 type="bind",
             ),
             Mount(
-                source="/mnt/OGD-DataExch/KaPo-Eventverkehr-St.Jakob",
+                source=f"{PATH_TO_CODE}/data-processing/{DAG_ID}/data_orig",
                 target="/code/data_orig",
                 type="bind",
             ),
@@ -71,5 +91,12 @@ with DAG(
                 target="/code/change_tracking",
                 type="bind",
             ),
+            Mount(
+                source=Variable.get("PATH_TO_CERTS"),
+                target="/certs",
+                type="bind",
+            ),
         ],
     )
+
+    cleanup_containers >> upload
