9fc687825e79ce72d3f2bec82fcc2e185d16554b
Orhan Saeedi <orhan.saeedi@bs.ch>
2026-06-30 10:54:46 +0200

    Update aue_luft_klybeck.py

--- aue_luft_klybeck.py
+++ aue_luft_klybeck.py
@@ -28,7 +28,7 @@ default_args = {
     "depend_on_past": False,
     "start_date": datetime(2026, 6, 9),
     "email": Variable.get("EMAIL_RECEIVERS"),
-    "email_on_failure": False,
+    "email_on_failure": True,
     "email_on_retry": False,
     "retries": 0,
     "retry_delay": timedelta(minutes=3),
