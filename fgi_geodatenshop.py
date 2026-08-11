a70e6dd30e1dc0304924c2cac18bd5d2df76f97a
Orhan Saeedi <orhan.saeedi@bs.ch>
2026-08-11 11:41:51 +0200

    Add Certs

--- fgi_geodatenshop.py
+++ fgi_geodatenshop.py
@@ -16,6 +16,10 @@ from docker.types import Mount
 
 from common_variables import COMMON_ENV_VARS, PATH_TO_CODE
 
+PATH_TO_LOCAL_CERTS = Variable.get("PATH_TO_LOCAL_CERTS")
+CA_ZID_FILENAME = Variable.get("CA_ZID_FILENAME")
+CA_PKI_FILENAME = Variable.get("CA_PKI_FILENAME")
+
 # DAG configuration
 DAG_ID = "fgi_geodatenshop"
 FAILURE_THRESHOLD = 1  # Skip first failure, fail on second
@@ -61,17 +65,32 @@ with DAG(
         api_version="auto",
         auto_remove="force",
         mount_tmp_dir=False,
-        command="uv run -m etl",
+        command="sh -c 'update-ca-certificates || true; uv run -m etl'",
         private_environment={
             **COMMON_ENV_VARS,
             "FTP_USER_01": Variable.get("FTP_USER_01"),
             "FTP_PASS_01": Variable.get("FTP_PASS_01"),
+            "CURL_CA_BUNDLE": "/etc/ssl/certs/ca-certificates.crt",
+            "NODE_EXTRA_CA_CERTS": "/etc/ssl/certs/ca-certificates.crt",
+            "PERL_LWP_SSL_CA_FILE": "/etc/ssl/certs/ca-certificates.crt",
+            "REQUESTS_CA_BUNDLE": "/etc/ssl/certs/ca-certificates.crt",
+            "SSL_CERT_FILE": "/etc/ssl/certs/ca-certificates.crt",
         },
         container_name=f"{DAG_ID}--upload",
         docker_url="unix://var/run/docker.sock",
         network_mode="bridge",
         tty=True,
         mounts=[
+            Mount(
+                source=f"{PATH_TO_LOCAL_CERTS}{CA_ZID_FILENAME}",
+                target=f"/usr/local/share/ca-certificates/{CA_ZID_FILENAME}",
+                type="bind",
+            ),
+            Mount(
+                source=f"{PATH_TO_LOCAL_CERTS}{CA_PKI_FILENAME}",
+                target=f"/usr/local/share/ca-certificates/{CA_PKI_FILENAME}",
+                type="bind",
+            ),
             Mount(
                 source="/mnt/OGD-DataExch/StatA/harvesters/FGI",
                 target="/code/data",
